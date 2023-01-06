d24eff2d1e8bdf0f79672bb61cc83e89185ff544
Joachim Løvgaard <joachim@loevgaard.dk>
2023-01-06 11:47:51 +0100

    Add filter for setono/coolrunner-php-sdk

--- composer-unused.php
+++ composer-unused.php
@@ -7,6 +7,7 @@ use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 
 return static function (Configuration $config): Configuration {
     return $config
+        ->addNamedFilter(NamedFilter::fromString('setono/coolrunner-php-sdk'))
         ->addNamedFilter(NamedFilter::fromString('symfony/http-client')) // Used as the HTTP client for the CoolRunner PHP SDK
     ;
 };
